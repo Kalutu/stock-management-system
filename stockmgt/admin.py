d4eb856806738ee4cd50606fa3622a3dedccd9d4
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-03 14:03:10 +0300

    Created a create form

--- stockmgt/admin.py
+++ stockmgt/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Stock
+from .forms import StockCreateForm
 
 # Register your models here.
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+   form = StockCreateForm
+   list_display = ['category', 'item_name', 'quantity']
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+
+admin.site.register(Stock, StockCreateAdmin)
