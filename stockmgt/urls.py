22d74c62d07bb96cbe4423941b1ffae7b125e5fe
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-04 12:07:34 +0300

    Update

--- stockmgt/urls.py
+++ stockmgt/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'stock'
+
 urlpatterns = [
     path('list/', list_items, name='list'),
+    path('add/', add_items, name='add'),
 ]
--- stockmgt/views.py
+++ stockmgt/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 
 # Create your views here.
 def list_items(request):
     form = Stock.objects.all()
     return render(request, 'stock/list.html', {'form':form})
+
+def add_items(request):
+    if request.method == 'POST':
+        form = StockCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('stock:list')
+    else:
+        form = StockCreateForm()
+    
+    return render(request, 'stock/add.html', {'form': form})
