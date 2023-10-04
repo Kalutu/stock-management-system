681ab748be688cdcedf56f5cbcbd5714c6eda43b
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-04 13:26:49 +0300

    Added authentication

--- stockmgt/views.py
+++ stockmgt/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'stock/home.html')
 
+@login_required(login_url=('/accounts/login'))
 def list_items(request):
     form = Stock.objects.all()
     return render(request, 'stock/list.html', {'form':form})
 
+@login_required(login_url=('/accounts/login'))
 def add_items(request):
     if request.method == 'POST':
         form = StockCreateForm(request.POST)
