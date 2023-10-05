b8abaf5b24bad189d241ac3db5fd9e431420a65a
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-05 09:40:13 +0300

    Form validation

--- stockmgt/forms.py
+++ stockmgt/forms.py
@@ -5,6 +5,21 @@ class StockCreateForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['category', 'item_name', 'quantity']
+        
+    def clean_item_name(self):
+          item_name = self.cleaned_data.get('item_name')
+          if not item_name:
+            raise forms.ValidationError("This field is required")
+          for instance in Stock.objects.all():
+              if instance.item_name == item_name:
+                raise forms.ValidationError(str(item_name) + ' is already created')
+          return item_name
+        
+    def clean_category(self):
+          category = self.cleaned_data.get('category')
+          if not category:
+            raise forms.ValidationError("This field is required")
+          return category
 
 class StockSearchForm(forms.ModelForm):
    class Meta:
